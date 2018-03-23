40b9533e97dc816c5095b5182682d1a3ea24de07
Dennis <Dennis@Dennis-DESKTOP-HP>
2018-03-23 11:54:49 +0100

    Merge origin/master
    Conflicts:
    	src/ro_gruppoe_project/RO_gruppoE_project.java

--- src/ro_gruppoe_project/RO_gruppoE_project.java
+++ src/ro_gruppoe_project/RO_gruppoE_project.java
@@ -5,6 +5,9 @@
  */
 package ro_gruppoe_project;
 
+import java.io.File;
+import javax.swing.JFileChooser;
+
 /**
  *
  * @author Dennis, Claudia
@@ -16,7 +19,16 @@ public class RO_gruppoE_project {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        selectFile();
+    }
+    
+    static public void selectFile(){
+        JFileChooser chooser = new JFileChooser();
         
+        int returnVal = chooser.showOpenDialog(null);
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+            System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
+        }
     }
     
 }
