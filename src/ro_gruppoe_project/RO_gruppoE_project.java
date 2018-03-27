90970b4ade3d463417ca840819604e9352ff9eaa
clodia92 <claudia.porcu92@gmail.com>
2018-03-27 11:03:18 +0200

    Depot and Vehicle

--- src/ro_gruppoe_project/RO_gruppoE_project.java
+++ src/ro_gruppoe_project/RO_gruppoE_project.java
@@ -16,6 +16,8 @@ import java.io.IOException;
  * @author Dennis, Claudia
  */
 public class RO_gruppoE_project {
+    Depot depot;
+    Customer [] customers;
 
     /**
      * @param args the command line arguments
