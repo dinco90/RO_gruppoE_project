306bacb70f8a1dc4cb0b726fa2d0b79fcc036fce
Dennis <Dennis@Dennis-DESKTOP-HP>
2018-03-27 11:04:51 +0200

    added lineCounter

--- src/ro_gruppoe_project/RO_gruppoE_project.java
+++ src/ro_gruppoe_project/RO_gruppoE_project.java
@@ -10,6 +10,10 @@ import javax.swing.JFileChooser;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -36,17 +40,31 @@ public class RO_gruppoE_project {
     }
 
     static private void readFile(String fileString) {
+
         BufferedReader br = null;
         FileReader fr = null;
+        int lineCounter = 0;
 
         try {
             fr = new FileReader(fileString);
+            
             br = new BufferedReader(fr);
 
             String sCurrentLine;
 
             while ((sCurrentLine = br.readLine()) != null) {
-                System.out.println(sCurrentLine);
+                lineCounter++;
+                // switch
+                switch(lineCounter){
+                    case 1:
+                        System.out.println(sCurrentLine);
+                        break;
+                    case 2:
+                        System.out.println(sCurrentLine);
+                        break;
+                }
+                //System.out.println(sCurrentLine);
+                
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -63,9 +81,10 @@ public class RO_gruppoE_project {
             }
 
         }
+        System.out.println(lineCounter);
     }
 
-    public double calculateDistance(Customer c1, Customer c2){
+    public double calculateDistance(Customer c1, Customer c2) {
         double d;
         d = Math.hypot(c1.getX() - c2.getX(), c1.getY() - c2.getY());
         return d;
