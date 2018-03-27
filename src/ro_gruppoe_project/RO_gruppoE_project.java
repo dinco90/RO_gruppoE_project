1b59355e90763e84d45020d58460fa29a4393a0d
Dennis <Dennis@Dennis-DESKTOP-HP>
2018-03-27 10:25:09 +0200

    added readFile

--- src/ro_gruppoe_project/RO_gruppoE_project.java
+++ src/ro_gruppoe_project/RO_gruppoE_project.java
@@ -7,6 +7,9 @@ package ro_gruppoe_project;
 
 import java.io.File;
 import javax.swing.JFileChooser;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
@@ -19,22 +22,53 @@ public class RO_gruppoE_project {
      */
     public static void main(String[] args) {
 
-        selectFile();
+        RO_gruppoE_project.readFile(RO_gruppoE_project.selectFile());
     }
-    
-    static private void selectFile(){
+
+    static private String selectFile() {
         JFileChooser chooser = new JFileChooser();
-        
+
         int returnVal = chooser.showOpenDialog(null);
-        if(returnVal == JFileChooser.APPROVE_OPTION) {
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
             System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
+        }
+        return chooser.getSelectedFile().getAbsolutePath();
+    }
+
+    static private void readFile(String fileString) {
+        BufferedReader br = null;
+        FileReader fr = null;
+
+        try {
+            fr = new FileReader(fileString);
+            br = new BufferedReader(fr);
+
+            String sCurrentLine;
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                System.out.println(sCurrentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
         }
     }
 
-    public double distance(Customer c1, Customer c2){
+    public double distance(Customer c1, Customer c2) {
         double d;
-        d = Math.hypot(c1.getX()-c2.getX(), c1.getY()-c2.getY());
+        d = Math.hypot(c1.getX() - c2.getX(), c1.getY() - c2.getY());
         return d;
     }
-    
+
 }
